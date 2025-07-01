f2241aece22de3224880d25c773eda1b729652c8
Suraj Rathour <110195289+SurajRathour24@users.noreply.github.com>
2025-07-01 22:40:13 +0530

    updated JS

---
+++ Full-JS/JS-1/script.js
@@ -0,0 +1,11 @@
+//var, let, const
+var playerName;   // Here i have declear the variable
+var playerName = 'Deepak Rathaur'; // Here i have initialize the value
+
+
+console.log(playerName);
+
+function myNum(){
+    let myNum1 = 100;
+} 
+console.log(myNum1);  // Here let will not accessible
