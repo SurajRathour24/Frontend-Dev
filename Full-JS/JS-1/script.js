24f5fdc42bfb9a32223a6cf95e6fd6acaad77287
Suraj Rathour <110195289+SurajRathour24@users.noreply.github.com>
2025-07-02 22:41:32 +0530

    Data Types

--- Full-JS/JS-1/script.js
+++ Full-JS/JS-1/script.js
@@ -1,11 +1,23 @@
 //var, let, const
-var playerName;   // Here i have declear the variable
-var playerName = 'Deepak Rathaur'; // Here i have initialize the value
+// var playerName;   // Here i have declear the variable
+// var playerName = 'Deepak Rathaur'; // Here i have initialize the value
 
 
-console.log(playerName);
+// console.log(playerName);
 
-function myNum(){
-    let myNum1 = 100;
-} 
-console.log(myNum1);  // Here let will not accessible
+// function myNum(){
+//     let myNum1 = 100;
+// } 
+// console.log(myNum1);  // Here let will not accessible
+
+//TDZ (Temporarly dead zone - Utna rea jitne mai JS ko ptaaa hai ki value in future hai toh hai pr abhi access ni kr skta 
+
+// Example 
+// console.log(myMoney);
+
+// let myMoney = 20000000000;
+
+console.log(player);
+
+let player = 'Snu';
+ 
---
+++ Full-JS/JS-2/script.js
@@ -0,0 +1,12 @@
+// Data types in JS 
+
+// 1. SSBBNNU 
+// Symbole, String, Number, Null, Boolean , bugint, Undefined 
+/*
+Data Types 
+1. Premitive 
+2. Reference 
+*/
+
+// Primitive -> In primitive we can see SVGSymbolElement, string, null, Number, Boolean , Bigint, Undefined  
+// Reference : Object , Array , Functions
