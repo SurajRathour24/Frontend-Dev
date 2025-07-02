bf3b0b861fd4697a18f5c9aea80dcdc3813a283b
Suraj Rathour <110195289+SurajRathour24@users.noreply.github.com>
2025-07-02 23:36:10 +0530

    primitive types

--- Full-JS/JS-2/script.js
+++ Full-JS/JS-2/script.js
@@ -10,3 +10,27 @@ Data Types
 
 // Primitive -> In primitive we can see SVGSymbolElement, string, null, Number, Boolean , Bigint, Undefined  
 // Reference : Object , Array , Functions
+
+/*
+
+1. String = '', "", ``;
+2. Number = 1,2,2121,
+3. Boolean = true, false
+4. Null = Null - Given By dev
+5. Undefined = undefined -> Given By JS engine
+6. Symbole = 
+7. bigint = 
+
+
+
+
+Primitive
+
+
+
+
+
+*/
+
+
+let a = Number.MAX_SAFE_INTEGER;
