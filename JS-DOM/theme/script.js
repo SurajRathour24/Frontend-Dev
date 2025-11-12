51d21a3eb0a7f35333ce53ee6ad2b55492e1e4cd
Suraj Rathour <110195289+SurajRathour24@users.noreply.github.com>
2025-11-12 23:30:48 +0530

    updated

--- JS-DOM/theme/script.js
+++ JS-DOM/theme/script.js
@@ -10,3 +10,25 @@ else{
     console.log('Theme is light ☀️');
 }
 
+let body = document.querySelector('body');
+let btn = document.querySelector('#darkModeBtn');
+
+console.log(body);
+
+let ligthDarkMode = (evnt) => {
+    if (osTheme == true){
+        body.classList.add('dark');
+        body.classList.remove('light');
+        console.log('Dark mode');
+    }
+    else{
+        body.classList.add('light');
+        body.classList.remove('dark');
+        console.log('Light mode');
+    }
+}
+
+ligthDarkMode();
+
+
+btn.addEventListener("click", ligthDarkMode);
